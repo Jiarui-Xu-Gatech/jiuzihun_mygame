aae7292daad930fb56a8a1b4881cb77481745238
jxu <xvjiarui@gmail.com>
2025-09-07 01:39:49 +0800

    complete mount pictures modification

--- character/jiuzisha.js
+++ character/jiuzisha.js
@@ -26358,7 +26358,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                 content:function(){
                     "step 0"
                     var card=get.cardPile2(function(card){
-                        return get.name(card)=='hualiu';
+                        return get.name(card)=='chitu';
                     });
                     if (card){
                         player.gain(card);
